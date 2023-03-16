0bf6bbda9abf8127723eedfd033dcbaf56eccee0
Brian Ballantine <bballantine@earnestresearch.com>
2023-03-16 10:38:37 -0400

    clean-up

--- draugr.go
+++ draugr.go
@@ -5,20 +5,71 @@ import (
 	"fmt"
 	"io/fs"
 	"log"
+	"os"
 	"path/filepath"
+	"sort"
 
 	"github.com/bballant/draugr/db"
+	"github.com/bballant/draugr/words"
 )
+
+func addsAndRems(newTokens []string, oldTokens []string) (adds []string, rems []string) {
+	sort.Strings(newTokens)
+	sort.Strings(oldTokens)
+	n := 0
+	o := 0
+	for {
+		if n == len(newTokens) {
+			rems = append(rems, oldTokens[o:]...)
+			break
+		}
+		if o == len(oldTokens) {
+			adds = append(adds, newTokens[n:]...)
+			break
+		}
+		nstr := newTokens[n]
+		ostr := oldTokens[o]
+		if nstr > ostr {
+			o++
+			rems = append(rems, ostr)
+		} else if ostr > nstr {
+			n++
+			adds = append(adds, ostr)
+		} else {
+			o++
+			n++
+		}
+	}
+	return adds, rems
+}
 
 func indexFile(db *db.DB, path string, info fs.FileInfo) error {
 	prevModTime := db.PathIndex.GetModTime(path)
 	currModTime := info.ModTime()
 	if !currModTime.After(prevModTime) {
-		log.Printf("%v is not after %v, not indexing %v\n", currModTime, prevModTime, path)
+		log.Printf("%v is not after %v, not indexing %s\n", currModTime, prevModTime, path)
 		return nil
 	}
-	db.PathIndex.SetModTime(path, currModTime)
+	if info.IsDir() {
+		log.Printf("not indexing dir %s\n", path)
+		return nil
+	}
+	bs, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
 	log.Printf("%v is after %v, indexing %v\n", currModTime, prevModTime, path)
+	newTokens := words.Tokenize(string(bs))
+	oldTokens := db.PathTermIndex.GetTerms(path)
+	fmt.Printf("new %v\n", newTokens)
+	fmt.Printf("old %v\n", oldTokens)
+	addToks, remToks := addsAndRems(newTokens, oldTokens)
+	fmt.Printf("add %v\n", addToks)
+	fmt.Printf("rem %v\n", remToks)
+	// old terms - current terms = terms to delete
+	// boost count for all add terms
+	// drop count for all delete terms
+	db.PathIndex.SetModTime(path, currModTime)
 	return nil
 }
 
@@ -41,6 +92,4 @@ func main() {
 		return nil
 	}
 	filepath.Walk(*dirFlag, walkFile)
-	filepath.Walk(*dirFlag, walkFile)
-	fmt.Println(db.PathIndex)
 }
